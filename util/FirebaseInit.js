f710a038e7f16901b31eb6720c69e07549093124
Kazuya Erdos <kazuya_erdos@brown.edu>
2024-09-12 15:28:09 -0400

    remove config vars

--- util/FirebaseInit.js
+++ util/FirebaseInit.js
@@ -4,16 +4,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// TODO: copy-paste the config variables found in your Firebase Project Settings!
 const firebaseConfig = {
-	apiKey: "AIzaSyD1E7KXJh11ZZGtbhSLZtxvJZxeheeBS8g",
-	authDomain: "my-app-45293.firebaseapp.com",
-	projectId: "my-app-45293",
-	storageBucket: "my-app-45293.appspot.com",
-	messagingSenderId: "970470178876",
-	appId: "1:970470178876:web:468921bed7a8acc76bc339",
-	measurementId: "G-B3BR3NGELF"
+	apiKey: "",
+	authDomain: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: "",
+	measurementId: ""
 };
 
 // Initialize Firebase
