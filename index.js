ed6c75a4ba802855e3dcf70c87edf141df56c570
Kazuya Erdos <kazuya_erdos@brown.edu>
2024-09-12 15:50:25 -0400

    add get and post ex

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv"
 dotenv.config()
 
 import { db } from "./util/FirebaseInit.js";
-import { collection } from "firebase/firestore"
+import { collection, getDocs, addDoc } from "firebase/firestore"
 
 const app = express()
 const port = 8080;
@@ -22,6 +22,38 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.get("/", async (req, res) => {
 	res.send("Hello World!");
 });
+
+// Get all students
+app.get("/students", async (req, res) => {
+	const collectionRef = collection(db, "Students");
+	const collectionSnap = await getDocs(collectionRef)
+	const docs = []
+	collectionSnap.forEach((doc) => {
+		docs.push(doc.data())
+	})
+	res.send(docs)
+})
+
+// Add a new student
+app.post("/students", async (req, res) => {
+	const studentRef = collection(db, "Students");
+	const studentData = {
+		firstName: "First",
+		lastName: "Last",
+		concentrations: [
+			"Concentration One",
+			"Concentration Two",
+		],
+		classYear: "2028",
+	}
+	try {
+		await addDoc(studentRef, studentData)
+	} catch (e) {
+		console.error(e)
+		res.send(500) // Error
+	}
+	res.send(200, "Succesfully Created Student")
+})
 
 function start() {
 	app.listen(port, () => {
--- util/FirebaseInit.js
+++ util/FirebaseInit.js
@@ -6,15 +6,14 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: copy-paste the config variables found in your Firebase Project Settings!
 const firebaseConfig = {
-	apiKey: "",
-	authDomain: "",
-	projectId: "",
-	storageBucket: "",
-	messagingSenderId: "",
-	appId: "",
-	measurementId: ""
+	apiKey: "AIzaSyD1E7KXJh11ZZGtbhSLZtxvJZxeheeBS8g",
+	authDomain: "my-app-45293.firebaseapp.com",
+	projectId: "my-app-45293",
+	storageBucket: "my-app-45293.appspot.com",
+	messagingSenderId: "970470178876",
+	appId: "1:970470178876:web:468921bed7a8acc76bc339",
+	measurementId: "G-B3BR3NGELF"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
