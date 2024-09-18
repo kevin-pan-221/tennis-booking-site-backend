3ed30cf12b76d4c4aad58f363c298091ba6352af
petearrpan <69205278+petearrpan@users.noreply.github.com>
2024-09-18 16:06:11 -0700

    Add support for deregistering a guest

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv"
 dotenv.config()
 
 import { db } from "./util/FirebaseInit.js";
-import { collection, getDocs, addDoc } from "firebase/firestore"
+import { collection, getDocs, addDoc, query, where, deleteDoc } from "firebase/firestore"
 
 const app = express()
 const port = 8080;
@@ -23,34 +23,18 @@ app.get("/", async (req, res) => {
 	res.send("Hello World!");
 });
 
-/*
-// Get all students
-app.get("/students", async (req, res) => {
-	console.log("getting all students")
-	const collectionRef = collection(db, "Students");
-	const collectionSnap = await getDocs(collectionRef)
-	const docs = []
-	collectionSnap.forEach((doc) => {
-		docs.push(doc.data())
-	})
-	res.send(docs)
-})
-*/
 
 // Get guest count
 app.get("/guest", async (req, res) => {
 	console.log("getting guest count")
 	const collectionRef = collection(db, "Guest Database");
 	const collectionSnap = await getDocs(collectionRef)
-	// const docs = []
-	// collectionSnap.forEach((doc) => {
-        // 		docs.push(doc.data())
-	// })
 	res.send(collectionSnap.size.toString())
 })
 
 // Add a new guest
 app.post("/guest", async (req, res) => {
+	console.log("adding a guest")
 	const guestRef = collection(db, "Guest Database");
 	const guestBody = req.body
 	try {
@@ -60,6 +44,30 @@ app.post("/guest", async (req, res) => {
 		res.status(500);
 	}
 	res.status(200).send("Succesfully added a guest.")
+})
+
+// Deregister guest
+app.post("/remove_guest", async (req, res) => {
+	console.log("removing a guest")
+	const guestInfoRef = collection(db, "Guest Database");
+	const phoneNumber = req.body.phoneNumber
+	try {   // Create a query to find the document(s)
+		const q = query(guestInfoRef, where("phoneNumber", "==", phoneNumber));
+
+		// Get the matching documents
+		const querySnapshot = await getDocs(q);
+
+		// Loop through the documents and delete them
+		querySnapshot.forEach((doc) => {
+			console.log(doc.data())
+			// doc.ref.delete();
+			deleteDoc(doc.ref);
+		});
+	} catch (e) {
+		console.error(e)
+		res.status(500);
+	}
+	res.status(200).send("Succesfully deregistered a guest.")
 })
 
 function start() {
